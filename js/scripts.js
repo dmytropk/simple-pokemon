01431df2d5d01064edee9f2ca237df5cb28eeb4a
kandrobat <51060880+kandrobat@users.noreply.github.com>
2019-06-11 20:28:13 -0700

    added repo with 3 characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-// alert('Wubba lubba dub dub!');
-var randomNumber = (Math.random() * 6) + 1;
-var diceRoll = Math.floor(randomNumber);
-document.write(diceRoll);
+var repository = [
+    {name: 'Rick Sanchez', age: 70, job: ['scientist', ' inventor']},
+    {name: 'Morty Smith', age: 14, job: ['student at Harry Herpson High School', ' Rick\u0027s helping hand']},
+    {name: 'Jerry Smith', age: 35, job: ['advertising agent (fired)', ' unknown assigned Galactic Federation employment (laid off after Federation collapse)', ' unemployed']}
+];
+
+for (var i = 0; i < repository.length; i++) {
+    if (repository[i].age == 70) {
+        document.write('<p>' + repository[i].name + ' (age: ' + repository[i].age + ') ' + repository[i].job + ' &#x2012Wow! Old, but cool!');
+    } else {
+        document.write('<p>' + repository[i].name + ' (age: ' + repository[i].age + ') ' + repository[i].job);
+
+    }
+}
+
